7e3c77569d0eaac84b6499e70120545e7a632f39
yiqianyao <2912401452@qq.com>
2020-01-17 16:39:04 +0800

    增加房屋部件的选择功能，并调整房屋部件的解析逻辑

---
+++ code/server/public/wallTexture/poolData.js
@@ -0,0 +1,42 @@
+let poolData = {     // 材质池
+    door:   [
+        '/src/images/room/door/1.jpg',
+        '/src/images/room/door/2.jpg',
+        '/src/images/room/door/3.jpg',
+        '/src/images/room/door/4.jpg',
+        '/src/images/room/door/5.jpg'
+    ],
+    win:    [
+        '/src/images/room/win/1.jpg',
+        '/src/images/room/win/2.jpg',
+        '/src/images/room/win/3.jpg',
+        '/src/images/room/win/4.jpg',
+        '/src/images/room/win/5.jpg'
+    ],
+    wall:   [
+        '/src/images/room/wall/1.jpg',
+        '/src/images/room/wall/2.jpg',
+        '/src/images/room/wall/3.jpg',
+        '/src/images/room/wall/4.jpg',
+        '/src/images/room/wall/5.jpg',
+        '/src/images/room/wall/6.jpg',
+        '/src/images/room/wall/7.jpg',
+        '/src/images/room/wall/8.jpg',
+        '/src/images/room/wall/9.jpg',
+        '/src/images/room/wall/10.jpg'
+    ],
+    roof:   [
+        '/src/images/room/roof/1.jpg',
+        '/src/images/room/roof/2.jpg',
+        '/src/images/room/roof/3.jpg',
+        '/src/images/room/roof/4.jpg',
+        '/src/images/room/roof/5.jpg'
+    ],
+    floor:  [
+        '/src/images/room/floor/1.jpg',
+        '/src/images/room/floor/2.jpg',
+        '/src/images/room/floor/3.jpg',
+        '/src/images/room/floor/4.jpg',
+        '/src/images/room/floor/5.jpg'
+    ]
+}
